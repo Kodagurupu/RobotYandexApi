7bec810d840bad42dc3da33d3e5fdbc2ecc6f6c1
Grisha Ivanetc <59262523+deCodedLife@users.noreply.github.com>
2020-07-07 00:14:07 +0300

    Save chache, bug fixes etc

--- yandexApi.go
+++ yandexApi.go
@@ -1,34 +1,76 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/azzzak/alice"
-	"github.com/gorilla/mux"
 )
 
-func main() {
+func fileExists(filename string) bool {
 
-	router := mux.NewRouter()
-	router.HandleFunc("/api/yandex", serve).Methods("POST")
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	return !info.IsDir()
+}
+
+func createDirectory(dirName string) bool {
+
+	src, err := os.Stat(dirName)
+
+	if os.IsNotExist(err) {
+
+		errDir := os.MkdirAll(dirName, 0755)
+		if errDir != nil {
+			panic(err)
+		}
+
+		return true
+	}
+
+	if src.Mode().IsRegular() {
+		log.Println(dirName, "already exist as a file!")
+		return false
+	}
+
+	return false
+}
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	createDirectory("Sessions")
+}
+
+func main() {
 	updates := alice.ListenForWebhook("/")
-	go http.ListenAndServeTLS(":3000", "server.crt", "server.key", router)
-	http.ListenAndServe(":8080", router)
+	go http.ListenAndServeTLS(":3000", "server.crt", "server.key", nil)
+
 	updates.Loop(func(k alice.Kit) *alice.Response {
 		req, resp := k.Init()
 
 		log.Printf("User send: " + req.OriginalUtterance())
 		requests = append(requests, req)
-
+		file, err := os.OpenFile("Sessions/"+req.UserID(), os.O_RDWR|os.O_CREATE, 777)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		marshaled, _ := json.Marshal(req)
+		file.WriteString(string(marshaled))
 		if req.IsNewSession() {
 			return resp.Text("Здравствуйте. " + firstMessage)
 		} else if itemExists(helpQuestions, req.Command()) {
 			return helpFunction(*resp)
 		} else if itemExists(abilityQuestions, req.Command()) {
 			return showPossibilities(*resp)
-		} else if itemExists(commands, req.Command()) {
+		} else if itemExists(controllCommands, req.Command()) {
 			return resp.Text("Выполняю")
+		} else if itemExists(stopCommands, req.Command()) {
+			return resp.Text("Конец потока").EndSession()
 		} else {
 			return resp.Text("Не поняла вопроса, переформулируйте его, и повторите снова")
 		}
